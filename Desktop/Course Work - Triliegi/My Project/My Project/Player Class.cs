2eec26e2b08743326790ea653545fd2fa66949f3
unknown <you@example.com>
2018-04-30 09:09:01 -0500

    More scaffolding

---
+++ Desktop/Course Work - Triliegi/My Project/My Project/Player Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Project
+{
+    class Player_Class
+    {
+
+        // member variables
+        public int score;
+        public bool computer = false;
+
+
+
+
+        // constructure 
+
+
+
+//  methods
+//public void TestForTwoPlayers()
+//{
+//    
+//    if USER INPUT = 2
+//    {run a two player game }
+//    else 
+//      {run a CPU along side/after player 1 goes}
